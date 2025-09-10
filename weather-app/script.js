f6b91d3a52eb9d81e8f6e7e0adfb73679b0ba1ab
pedro-pgam <pedropaulogam@gmail.com>
2025-09-11 00:48:11 +0200

    Update script.js

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,6 +1,6 @@
 async function getWeather() {
   const city = document.getElementById('city').value;
-  const apiKey = "SUA_API_KEY"; // Coloque sua chave da OpenWeatherMap
+  const apiKey = "YOUR_API_KEY"; // Put your OpenWeatherMap API key here
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
